305ca5a9ac2b78aaf0888ea4442a0702fbc66552
Jarmo Riikonen <jarmo.riikonen@posti.com>
2025-07-04 10:27:29 +0300

    get changed files

---
+++ get-sceptre-files.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env bun
+
+/**
+ * @module This script will get all sceptre files that has changed
+ */
+
+import core from '@actions/core'
+import github from '@actions/github'
+
+async function run() {
+  const token = core.getInput('github-token', { required: true })
+  const owner = core.getInput('owner', { required: true })
+  const repository = core.getInput('repository', { required: true })
+  const pr = core.getInput('pr', { required: true })
+  const octokit = github.getOctokit(token)
+
+  const { data: pullRequest } = await octokit.rest.pulls.get({
+    owner: owner,
+    repo: repository,
+    pull_number: Number(pr),
+  })
+
+  console.log(pullRequest)
+}
+
+run()
